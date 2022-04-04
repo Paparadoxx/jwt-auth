3ced280018b95ead4c46e7c9036197dff0861bf5
Alexander Bolshakov <paparradoxx@gmail.com>
2022-04-04 12:33:17 +0400

    added tokens, token, user models. user controller

---
+++ server/dtos/user.dto.js
@@ -0,0 +1,11 @@
+module.export = class UserDto{
+	email;
+	id;
+	isActivated;
+
+	constructor(model){
+		this.email = model.email;
+		this.id = model._id;
+		this.isActivated = model.isActivated;
+	}
+}
