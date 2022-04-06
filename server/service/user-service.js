c5db3b50d18e3d95ae5ffc7407ecce17cd7d84fe
Alexander Bolshakov <paparradoxx@gmail.com>
2022-04-06 17:09:40 +0400

    added mail-service. mail validation, modified token and user services

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -3,23 +3,24 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
-const UserDto = require('../dtos/user.dto');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
 	async registration (email, password){
-		const candidaie = await UserModel.findOne({email})
+		const candidate = await UserModel.findOne({email})
 		if (candidate){
 			throw new Error(`Пользователь с адресом ${email} уже существует`)
 		}
 		const hashPassword = await bcrypt.hash(password, 3);
 		const activationLink = uuid.v4();
+
 		const user = await UserModel.create({email, password: hashPassword, activationLink})
-		await mailService.sendActivationMail(email, activationLink);
+		await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
 		const userDto = new UserDto(user); //id, email, isActivated
 		const tokens = tokenService.generateTokens({...userDto});
-		await tokenService.saveToken(userDto, tokens.refreshToken);
-
+		await tokenService.saveToken(userDto.id, tokens.refreshToken);
+		
 		return {...tokens, user: userDto}
 	}
 }
